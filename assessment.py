7fd3e67bf5c1d0e5ebe8efbbbb29cdfc18e45880
tamunoWoks <tamunosakiwokoma86@gmail.com>
2024-07-12 22:52:09 -0600

    Ask if the user is ready for the test and their field

--- assessment.py
+++ assessment.py
@@ -5,3 +5,19 @@ class EngineeringTest:
     def __init__(self):
         # Initialize the score to zero
         self.score = 0
+
+
+        def main(self):
+            print("Welcome to the Engineering Internship assessment Test!\n")
+
+            # Ask if the user is ready for the test
+            play = input("Are you ready for your test? ").strip()
+            self.ready(play)
+
+            # Ask the user for their field of engineering
+            dept = input("\nWhat is your field of Engineering? ").strip()
+            field_name = self.field(dept)
+            print(
+                f"welcome to your {field_name} Engineering Assessment Test!!!"
+                "Choose the correct option among options A, B, C and D"
+            )
